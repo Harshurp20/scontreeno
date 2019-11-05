22725dd05646b187872f30d676206781a1129997
ivaskuu <ivascuadrian9@gmail.com>
2019-11-05 17:15:35 +0100

    Simple text in home_page.dart to test nfc

--- app/lib/pages_app/home/home_page.dart
+++ app/lib/pages_app/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scontreeno/managers/nfc_manager.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,10 +7,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _hasNfc;
+  String _nfcData;
+
+  @override
+  void initState() {
+    super.initState();
+    _initNfc();
+  }
+
+  void _initNfc() async {
+    _hasNfc = await NfcManager.hasNfc();
+    print('Has NFC: $_hasNfc');
+    setState(() {});
+
+    if (_hasNfc ?? false) {
+      NfcManager.listenNfc().listen((data) {
+        print(data.content);
+        setState(() {
+          _nfcData = data.content;
+        });
+      });
+    }
+  }
+
+  void _scanQR() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Has NFC: $_hasNfc'),
+          Text('NFC data: $_nfcData'),
+        ],
+      ),
     );
   }
 }
